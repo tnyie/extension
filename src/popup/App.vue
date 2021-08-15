47a99cd2075d8a2d3f3f2cbd3556dfa3c370bda7
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-08-15 15:28:31 +0100

    initialized extension

---
+++ src/popup/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="btn waves-effect waves-light">Click me</button>
+  
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: { HelloWorld }
+}
+</script>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto);
+
+html {
+  width: 400px;
+  height: 600px;
+  background: #2a2a2a;
+  color: white;
+  font-family: Roboto;
+}
+</style>
